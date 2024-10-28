4ca7287fccb22147eacb7d5a4d4b4b5ee2dcb789
Sam Taseff <2004.seraph@proton.me>
2024-10-28 20:23:17 +0000

    split gpu requirements

--- start-command.sh
+++ start-command.sh
@@ -1,2 +1,4 @@
- 
+#!/usr/bin/env bash
+
 alias ros='xhost +local:docker && sudo docker compose -f ./docker-compose.yml up -d --no-recreate && sudo docker exec -it ros-noetic-dev-box bash'
+alias ros-nvidia='xhost +local:docker && sudo docker compose -f ./docker-compose-nvidia.yml up -d --no-recreate && sudo docker exec -it ros-noetic-dev-box bash'
