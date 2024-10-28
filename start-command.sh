38fe5b23d993572e5a3cda80fca96a0a24bf1186
Sam Taseff <2004.seraph@proton.me>
2024-10-28 21:19:45 +0000

    build non tty

--- start-command.sh
+++ start-command.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+alias ros-non-tty='xhost +local:docker && sudo docker compose -f ./docker-compose.yml up -d --no-recreate && sudo docker exec ros-noetic-dev-box bash'
 alias ros='xhost +local:docker && sudo docker compose -f ./docker-compose.yml up -d --no-recreate && sudo docker exec -it ros-noetic-dev-box bash'
 alias ros-nvidia='xhost +local:docker && sudo docker compose -f ./docker-compose-nvidia.yml up -d --no-recreate && sudo docker exec -it ros-noetic-dev-box bash'
